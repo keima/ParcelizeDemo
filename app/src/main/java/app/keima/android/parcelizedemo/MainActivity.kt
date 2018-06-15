b53b115b9ccf3359398f6b6e7bc8ac6e5a4e138f
Kouta Imanaka <imanaka-kota@cyberagent.co.jp>
2018-06-15 20:34:19 +0900

    wip

---
+++ app/src/main/java/app/keima/android/parcelizedemo/MainActivity.kt
@@ -0,0 +1,22 @@
+package app.keima.android.parcelizedemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import app.keima.android.parcelizedemo.databinding.ActivityMainBinding
+
+class MainActivity: AppCompatActivity() {
+
+  private lateinit var binding : ActivityMainBinding
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+    binding.saveButton.setOnClickListener {
+
+    }
+
+  }
+
+}
---
+++ app/src/main/java/app/keima/android/parcelizedemo/Mp3Tag.kt
@@ -0,0 +1,3 @@
+package app.keima.android.parcelizedemo
+
+class Mp3Tag
